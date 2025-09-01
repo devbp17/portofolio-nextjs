5435d48a605c14e6c4316c4bff1e8050f233a506
Your Name <you@example.com>
2025-09-01 19:15:36 +0700

    fix

--- src/app/component/navbar.tsx
+++ src/app/component/navbar.tsx
@@ -7,18 +7,18 @@ import { usePathname } from 'next/navigation';
 const navStyle = `text-white m-[5px] text-[20px] bg-black border-2 border-gray-500
                   rounded-[30px] w-[88px] flex items-center justify-center transition-all
                   duration-500 ease-in-out hover:text-gray-500 `
-
+const active = `bg-gray-700 text-white border-white`
 
 function Navbar() {
   const pathname = usePathname();
   return (
     <div>
       <nav className='p-6 flex justify-end items-center'>
-        <Link className={`${navStyle} ${pathname === "/" }`} href={"/"}>Home</Link>
-        <Link className={`${navStyle} ${pathname === "/about"}`} href={"/about"}>About</Link>
-        <Link className={`${navStyle} ${pathname === "/project"}`} href={"/project"}>Project</Link>
-        <Link className={`${navStyle} ${pathname === "/skills"}`} href={"/skills"}>Skills</Link>
-        <Link className={`${navStyle} ${pathname === "/skills"}`} href={"/contact"}>Contact</Link>
+        <Link className={`${navStyle} ${pathname === "/" ? active : "" }`} href={"/"}>Home</Link>
+        <Link className={`${navStyle} ${pathname === "/about" ? active : ""}`} href={"/about"}>About</Link>
+        <Link className={`${navStyle} ${pathname === "/project" ? active : ""}`} href={"/project"}>Project</Link>
+        <Link className={`${navStyle} ${pathname === "/skills" ? active : ""}`} href={"/skills"}>Skills</Link>
+        <Link className={`${navStyle} ${pathname === "/contact" ? active : ""}`} href={"/contact"}>Contact</Link>
       </nav>
     </div>
   )
